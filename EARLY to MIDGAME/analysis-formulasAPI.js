c42434b4fa3a9705da3e33d0f6e5c3998fc1f41f
Geoffrey F. Clark <97141856+GeoffreyFClark@users.noreply.github.com>
2023-12-17 19:16:03 -0500

    Added other tests using Formulas API

--- EARLY to MIDGAME/analysis-formulasAPI.js
+++ EARLY to MIDGAME/analysis-formulasAPI.js
@@ -83,33 +83,98 @@ export async function main(ns) {
         let gp4 = ns.formulas.hacking.growPercent(s_obj, 250, me, 4);
         let gp5 = ns.formulas.hacking.growPercent(s_obj, 500, me, 4);
         let gp6 = ns.formulas.hacking.growPercent(s_obj, 1000, me, 4);
-        
-        serverMaxMoneys[s] = s_mm;
+        // ns.formulas.mockServer.
 
-        
-        ns.tprint(`${s} req hacking level: ${hl}`);
-
+        // ns.tprint(``);
         // ns.tprint(`${s}:`);
         // ns.tprint(`hack Exp: ${hx}`);
-        // ns.tprint(`grow Time: ${gt}`);
+        ns.tprint(`grow Time: ${gt}`);
         // ns.tprint(`grow threads: ${gr} for current money ${s_ma} of max ${s_mm}`);
-        // ns.tprint(`weakenTime: ${wt}`);
+        ns.tprint(`weakenTime: ${wt}`);
         // ns.tprint(`!Hack Percent: ${hp}`);
         // ns.tprint(`hack Chance: ${hc}`);
-        // ns.tprint(`hackTime: ${ht}`);
+        ns.tprint(`hackTime: ${ht}`);
         // ns.tprint(`growPercent 500t 3c: ${gp1}`);
         // ns.tprint(`growPercent 500t 4c: ${gp2}`);
         // ns.tprint(`growPercent 500t 5c: ${gp3}`);
         // ns.tprint(`growPercent 250t 4c: ${gp4}`);
         // ns.tprint(`growPercent 500t 4c: ${gp5}`);
         // ns.tprint(`growPercent 1000t 4c: ${gp6}`);
-        
-    }
 
-    var sortableSMM = Object.entries(serverMaxMoneys);
-    sortableSMM.sort((a, b) => b[1] - a[1]);
-    sortableSMM.forEach(entry => {
-        ns.tprint('Server Name:', entry[0], 'Max Money:', entry[1]);
-    });
+        // serverMaxMoneys[s] = s_mm;
+        // ns.tprint(`${s} req hacking level: ${hl}`);
+    }
+    
+    let z = ns.getServer("ecorp");
+    ns.tprint(z);
+
+    z["moneyAvailable"] = 0;
+    let g = ns.formulas.hacking.growPercent(z, 1000, me, 6)
+    ns.tprint(`1k threads, 0 money, grow: ${g}`);
+    g = ns.formulas.hacking.growPercent(z, 2000, me, 6)
+    ns.tprint(`2k threads, 0 money, grow: ${g};`);
+    g = ns.formulas.hacking.growPercent(z, 3000, me, 6)
+    ns.tprint(`3k threads, 0 money, grow: ${g};`);
+    g = ns.formulas.hacking.growPercent(z, 4000, me, 6)
+    ns.tprint(`4k threads, 0 money, grow: ${g};`);
+
+    z["moneyAvailable"] = ns.getServerMaxMoney("ecorp")/2;
+    g = ns.formulas.hacking.growPercent(z, 1000, me, 6)
+    ns.tprint(`1k threads, 1/2 money, grow: ${g};`);
+    g = ns.formulas.hacking.growPercent(z, 2000, me, 6)
+    ns.tprint(`2k threads, 1/2 money, grow: ${g};`);
+    g = ns.formulas.hacking.growPercent(z, 3000, me, 6)
+    ns.tprint(`3k threads, 1/2 money, grow: ${g};`);
+    g = ns.formulas.hacking.growPercent(z, 4000, me, 6)
+    ns.tprint(`4k threads, 1/2 money, grow: ${g};`);
+    
+    z["moneyAvailable"] = ns.getServerMaxMoney("ecorp") * (3/4);
+    g = ns.formulas.hacking.growPercent(z, 1000, me, 6)
+    ns.tprint(`1k threads, 3/4 money, grow: ${g};`);
+    g = ns.formulas.hacking.growPercent(z, 2000, me, 6)
+    ns.tprint(`2k threads, 3/4 money, grow: ${g};`);
+    g = ns.formulas.hacking.growPercent(z, 3000, me, 6)
+    ns.tprint(`3k threads, 3/4 money, grow: ${g};`);
+    g = ns.formulas.hacking.growPercent(z, 4000, me, 6)
+    ns.tprint(`4k threads, 3/4 money, grow: ${g};`);
+
+    z["moneyAvailable"] = ns.getServerMaxMoney("ecorp") * (0.9);
+    g = ns.formulas.hacking.growPercent(z, 1000, me, 6)
+    ns.tprint(`1k threads, 90% money, grow: ${g};`);
+    g = ns.formulas.hacking.growPercent(z, 2000, me, 6)
+    ns.tprint(`2k threads, 90% money, grow: ${g};`);
+    g = ns.formulas.hacking.growPercent(z, 3000, me, 6)
+    ns.tprint(`3k threads, 90% money, grow: ${g};`);
+    g = ns.formulas.hacking.growPercent(z, 4000, me, 6)
+    ns.tprint(`4k threads, 90% money, grow: ${g};`);
+
+    z["moneyAvailable"] = ns.getServerMaxMoney("ecorp") * (0.9);
+    let h = ns.formulas.hacking.hackPercent(z, me);
+    ns.tprint(`100 thread hack: ${h*100}`);
+    ns.tprint(`100 thread hack: ${h*200}`);
+    ns.tprint(`100 thread hack: ${h*300}`);
+    ns.tprint(`100 thread hack: ${h*400}`);
+    ns.tprint(`100 thread hack: ${h*500}`);
+
+
+    // ns.getServerMaxMoney
+    // ns.getServerMoneyAvailable
+    // ns.getServerMinSecurityLevel
+    // ns.getServerSecurityLevel
+    // ns.formulas.hacking.growThreads(z, me, serverMaxMoneys, 6)
+    // ns.security
+    // ns.
+
+    
+
+    // let simObj = ns.formulas.mockServer.apply("ecorp");
+    
+    // ns.tprint(simObj);
+
+    // var sortableSMM = Object.entries(serverMaxMoneys);
+    // sortableSMM.sort((a, b) => b[1] - a[1]);
+    // sortableSMM.forEach(entry => {
+    //     ns.tprint('Server Name:', entry[0], 'Max Money:', entry[1]);
+    // });
     
 }
